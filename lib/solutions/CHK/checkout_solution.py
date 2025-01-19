662549d94cc0d2f9fcf4c1ad65340807ed66a2bb
sbx_user1051 <sbx_user1051@169.254.6.233>
2025-01-19 13:51:50 +0000

    Sun Jan 19 13:51:50 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,3 +2,34 @@
 # skus = unicode string
 def checkout(skus):
     allowed_skus = {"A": 50, "B": 30, "C": 20, "D": 15}
+    offers = {"A": (3, 130), "B": (2, 45)}
+    substrings = skus.split()
+    input_dict = {}
+    total = 0
+
+    for substring in substrings:
+        digits = ""
+        letter = ""
+
+        for char in substring:
+            if char.isdigit():
+                digits += char
+            else:
+                letter = char.upper()
+
+            if letter in allowed_skus:
+                input_dict[letter] = int(digits)
+            else:
+                return -1
+
+    for k, v in input_dict.items():
+        if k in offers:
+            offer_quant, quant_total = offers.get(k)
+            remainder = v % offer_quant
+            total += (remainder * allowed_skus.get(k)) + (quant_total * (v - remainder))
+        else:
+            price = allowed_skus.get(k)
+            total += price * v
+
+    return total
+
